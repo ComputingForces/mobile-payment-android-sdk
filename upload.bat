04bd9034aa007197e142074ef3a83a7bf535c729
elephant <darkhipov@webmoney.ru>
2016-12-02 13:41:22 +0300

    #settings

--- upload.bat
+++
@@ -1,6 +0,0 @@
-rem echo "# mobile-payment-android-sdk" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/ComputingForces/mobile-payment-android-sdk.git
-git push -u origin master
